25c7e94af247d79d582f4626f1f1e9fb9ff08e11
Adam Fortune <adamfortune96@protonmail.com>
2024-02-09 18:28:38 -0600

    Add serde_json dependency

--- jq/src/main.rs
+++ jq/src/main.rs
@@ -1,11 +1,18 @@
+use std::{io::{stderr, Write}, process::ExitCode};
 
-use std::{io::{stderr, stdin, BufReader, Read, Write}, process::{exit, ExitCode, ExitStatus}};
+use serde_json::Value;
 
 fn main() -> Result<(), ExitCode> {
     let args = dq_core::build_command("jq");
     let input = dq_core::parse_std_in()?;
 
-    
+    let parsed: Value = match serde_json::from_str(&input) {
+        Err(err) => {
+            stderr().write(format!("Failed to parse JSON. Message: {}", err).as_bytes()).ok();
+            return Err(ExitCode::FAILURE)
+        }
+        Ok(value) => value,
+    };
 
     return Ok(());
 }
