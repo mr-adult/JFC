95675c4a92cbd30cd01a8f0ba1d71f9e56ec01e1
Adam Fortune <adamfortune96@protonmail.com>
2024-02-20 22:31:53 -0600

    Enhancement: Recursive string parsing in the JSON

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,42 +2,18 @@
 #![doc = include_str!("../README.md")]
 
 use std::{
-    fs::OpenOptions, io::{stderr, stdin, stdout, BufRead, BufReader, Read, Write}, process::ExitCode
+    error::Error,
+    fs::OpenOptions,
+    io::{stderr, stdin, stdout, BufRead, BufReader, Read, Write},
+    process::ExitCode,
 };
 
 use clap::{command, Arg, ArgAction};
-use toy_json_formatter::{format, parse, FormatOptions};
+use toy_json_formatter::{format, parse, parser::{JsonString, Value}, FormatOptions};
 
 fn main() -> Result<(), ExitCode> {
     let arg_matches = command!()
-        .arg(
-            Arg::new("compact")
-                .long("compact")
-                .short('c')
-                .action(ArgAction::SetTrue)
-                .help("compact instead of pretty-printed output"),
-        )            
-        .arg(
-            Arg::new("raw")
-                .long("raw")
-                .short('r')
-                .action(ArgAction::SetTrue)
-                .help("tells jfc not to insert any new tokens to fix the output. Instead, only whitespace will be inserted into the output")
-        )
-        .arg(
-            Arg::new("file")
-                .long("file")
-                .short('f')
-                .help("read the input as a file path instead of a JSON string")
-        )
-        .arg(
-            Arg::new("tab")
-                .long("tab")
-                .action(ArgAction::SetTrue)
-                .help("use tabs for indentation (default: 2 spaces)"),
-        )
-        .after_help(
-            r#"
+        .about(r#"
                  ___  ________ ________     
                 |\  \|\  _____\\   ____\    
                 \ \  \ \  \__/\ \  \___|    
@@ -53,54 +29,128 @@ characters from the input. Any errors found along the way are emitted
 to standard error. In raw mode, no characters from the input are (or
 at least should be) dropped. If you see characters being dropped in 
 raw mode, please submit an issue to https://github.com/mr-adult/JFC.
-"#,
+"#)
+        .arg(
+            Arg::new("compact")
+                .long("compact")
+                .short('c')
+                .action(ArgAction::SetTrue)
+                .help("compact instead of pretty-printed output"),
+        )
+        .arg(
+            Arg::new("raw")
+            .long("raw")
+                .short('r')
+                .action(ArgAction::SetTrue)
+                .help("tells jfc not to insert any new tokens to fix the output. Instead, only whitespace will be inserted into the output")
+        )
+        .arg(
+            Arg::new("file")
+                .long("file")
+                .short('f')
+                .help("read the input as a file path instead of a JSON string")
+        )
+        .arg(
+            Arg::new("recursive")
+                .long("recursive")
+                .short('R')
+                .action(ArgAction::SetTrue)
+                .help("[non-raw mode only] pass each string in the output back through the parser to be parsed as JSON; this process is done recursively")
+        )
+        .arg(
+            Arg::new("tab")
+                .long("tab")
+                .action(ArgAction::SetTrue)
+                .help("use tabs for indentation (default: 2 spaces)"),
         )
         .get_matches();
 
     let compact = arg_matches.get_one("compact").unwrap_or(&false);
     let raw = arg_matches.get_one("raw").unwrap_or(&false);
-    let empty_string = String::with_capacity(0);                                                                                                    
-    let file = arg_matches.get_one::<String>("file").unwrap_or(&empty_string);
-    
+    let empty_string = String::with_capacity(0);
+    let file = arg_matches
+        .get_one::<String>("file")
+        .unwrap_or(&empty_string);
+    let recursive = arg_matches.get_one::<bool>("recursive").unwrap_or(&false);
+
     let tab = arg_matches.get_one::<bool>("tab").unwrap_or(&false);
     let indent_str = if *tab { "\t" } else { "  " };
-   
+
     let input = if file.is_empty() {
-        let reader = BufReader::new(stdin().lock());                                                                                     
+        let reader = BufReader::new(stdin().lock());
         read_buf_to_string(reader)?
     } else {
-        match OpenOptions::new()
-            .read(true)
-            .write(false)
-            .open(file) {
-                Ok(file) => {
-                    let reader = BufReader::new(file);
-                    read_buf_to_string(reader)?
-                } Err(err) => {
-                    let mut stderr = stderr().lock();
-                    stderr.write_all(format!("Failed to open file '{}'. Message: {}", file, err).as_bytes()).ok();
-                    stderr.write(&[b'\n']).ok();
-                    stderr.flush().ok();
-                    return Err(ExitCode::FAILURE)
-                }
+        match OpenOptions::new().read(true).write(false).open(file) {
+            Ok(file) => {
+                let reader = BufReader::new(file);
+                read_buf_to_string(reader)?
             }
+            Err(err) => {
+                let mut stderr = stderr().lock();
+                stderr
+                    .write_all(
+                        format!("Failed to open file '{}'. Message: {}", file, err).as_bytes(),
+                    )
+                    .ok();
+                stderr.write(&[b'\n']).ok();
+                stderr.flush().ok();
+                return Err(ExitCode::FAILURE);
+            }
+        }
     };
 
     let errs_to_report;
+    let mut errs_from_recursive_parsing: Vec<(String, Vec<Box<dyn Error>>)> = Vec::with_capacity(0);
     if *raw {
-        let (output, errs) = format(&input, Some(FormatOptions { compact: *compact, indent_str }));
+        let (output, errs) = format(
+            &input,
+            Some(FormatOptions {
+                compact: *compact,
+                indent_str,
+            }),
+        );
         let mut stdout = stdout().lock();
         stdout.write_all(output.as_bytes()).ok();
         stdout.write(&[b'\n', b'\n']).ok();
         stdout.flush().ok();
         errs_to_report = errs;
     } else {
-        let (output, errs) = parse(&input);
+        let (mut output, errs) = parse(&input);
+
+        if *recursive {
+            let mut stack = output
+                .get_all_leaves_iter_mut()
+                .filter(|value| matches!(value, Value::String(_)))
+                .collect::<Vec<_>>();
+
+            while let Some(top) = stack.pop() {
+                // We need this for basically the duration of the program, so just leak 
+                // it and let the OS clean up after us.
+                let string: &'static str = Box::leak(Box::new(JsonString::unescape(&top.to_string()).to_string()));
+                let (result, errs) = parse(string);
+                *top = result;
+                errs_from_recursive_parsing.push((top.to_string(), errs));
+                
+                // First value is top itself. We don't want infinite loops, so skip 1.
+                for value in top.get_all_leaves_iter_mut().skip(1) {
+                    if let Value::String(_) = value {
+                        stack.push(value);
+                    }
+                }
+            }
+        }
+
         let mut stdout = stdout().lock();
         if *compact {
             stdout.write_all(output.to_string().as_bytes()).ok();
         } else {
-            stdout.write_all(output.to_string_pretty().as_bytes()).ok();
+            stdout
+                .write_all(
+                    output
+                        .to_string_pretty_with_indent_str(if *tab { "\t" } else { "  " })
+                        .as_bytes(),
+                )
+                .ok();
         }
         stdout.write(&[b'\n', b'\n']).ok();
         stdout.flush().ok();
@@ -108,13 +158,49 @@ raw mode, please submit an issue to https://github.com/mr-adult/JFC.
     }
 
     if !errs_to_report.is_empty() {
-        let err_out = errs_to_report.into_iter().map(|err| format!("{}", err)).collect::<Vec<_>>().join("\n");
+        let err_out = errs_to_report
+            .into_iter()
+            .map(|err| format!("{}", err))
+            .collect::<Vec<_>>()
+            .join("\n");
+
         let mut stderr = stderr().lock();
         stderr.write_all(err_out.as_bytes()).ok();
         stderr.write_all(&[b'\n', b'\n']).ok();
         stderr.flush().ok();
     }
-    
+
+    if !errs_from_recursive_parsing.is_empty() {
+        let err_out = errs_from_recursive_parsing
+            .into_iter()
+            .map(|(json, errs)| {
+                if errs.is_empty() {
+                    return None;
+                }
+                let mut report =
+                    "Found the following errors while parsing the string ".to_string();
+                if json.len() > 70 {
+                    report.push_str(&json[0..70]);
+                    report.push_str("...");
+                } else {
+                    report.push_str(&json);
+                }
+                report.push_str("\n");
+                for err in errs {
+                    report.push_str(&format!("{}\n", err));
+                }
+                Some(report)
+            })
+            .flat_map(|opt| opt)
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        let mut stderr = stderr().lock();
+        stderr.write_all(err_out.as_bytes()).ok();
+        stderr.write_all(&[b'\n', b'\n']).ok();
+        stderr.flush().ok();
+    }
+
     Ok(())
 }
 
